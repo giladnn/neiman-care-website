a5b96b4df89f2ec14f6e1c740d2b9bd8c09f6077
giladnn <gilad.neimann@gmail.com>
2025-04-18 16:24:53 +0300

    remove blog for now

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -21,7 +21,7 @@ const Index = () => {
       <TestimonialsSection />
       <VideoSection />
       <AppointmentSection />
-      <BlogSection />
+      {/* <BlogSection /> */}
       <NewsSection />
     </Layout>
   );
